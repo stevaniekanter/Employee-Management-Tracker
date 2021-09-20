8341e78981a52a10df1c9148c2ec892334036ef3
Stevanie Kanter <stevaniek@yahoo.com>
2021-09-19 21:48:28 -0400

    updated

--- index.js
+++ index.js
@@ -4,5 +4,21 @@ const mysql = require('mysql2');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// express middleware
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
+
+// connect to dabase
+const db = mysql.createConnection({
+    host: "localhost",
+    port: process.env.PORT || 3001,
+    user: "root",
+    password: "track123",
+    database: "employee_tracker",
+  },
+    console.log(`Connected to employee_tracker database`)
+);
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
