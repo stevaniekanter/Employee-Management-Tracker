31040fde69f32a56af8ade783fbaa6bd918dd458
Stevanie Kanter <stevaniek@yahoo.com>
2021-09-23 15:47:14 -0400

    updated sql

--- db/schema.sql
+++ db/schema.sql
@@ -4,23 +4,30 @@ CREATE DATABASE employee_tracker;
 USE employee_tracker;
 
 CREATE TABLE department (
-	id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     dept_name VARCHAR(30) NOT NULL,
     PRIMARY KEY department(id)
 );
 
-CREATE TABLE role (
+CREATE TABLE employee_role (
     id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL(8,2) NOT NULL,
-    dept_id INT NOT NULL,
-    PRIMARY KEY role(id)
+    salary DECIMAL NOT NULL,
+    department_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
 );
 
+
 CREATE TABLE employee (
-    id INT AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
-    PRIMARY KEY employee(id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id)
+    REFERENCES employee_role(id)
+    ON DELETE SET NULL,
 );
